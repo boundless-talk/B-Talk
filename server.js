0733567bf333fdfc530176c16df14ce12ec94e44
freesia9353-prog <freesia9353@gmail.com>
2026-06-21 22:21:21 +0900

    Fix Gmail SMTP: use port 465 with SSL

--- server.js
+++ server.js
@@ -215,7 +215,9 @@ app.post('/sendVerifyEmail', async (req, res) => {
     }
 
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_PASS }
     });
 
